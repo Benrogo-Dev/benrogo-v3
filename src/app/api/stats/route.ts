bf24052ab5038d3c353b2d5fe2f66ef83ab588fa
BenRogozinski <brogodev@gmail.com>
2025-08-11 22:15:46 -0500

    Test 2

--- src/app/api/stats/route.ts
+++ src/app/api/stats/route.ts
@@ -1,12 +1,23 @@
+
 import { NextRequest } from "next/server";
 import { BigQuery } from "@google-cloud/bigquery";
 
-//export const runtime = "edge";
-
 export const GET = async (request: NextRequest): Promise<Response> => {
+  // Load credentials from environment variable
+  const credentials = process.env.GOOGLE_CREDENTIALS ? JSON.parse(process.env.GOOGLE_CREDENTIALS) : undefined;
   const bigQueryClient = new BigQuery({
-    keyFile: "./credentials.json"
+    credentials,
+    projectId: credentials?.project_id,
   });
 
-  return Response.json({test: 123}, {status: 200});
+  const query = `SELECT SUM(requestCount) AS requests, SUM(totalRequestLength) AS bytesIn, SUM(totalResponseLength) AS bytesOut FROM proxyAnalytics.combinedStats`;
+
+  const [statsJob] = await bigQueryClient.createQueryJob({
+    query,
+    location: "US"
+  });
+
+  const [[stats]] = await statsJob.getQueryResults();
+
+  return Response.json({stats}, {status: 200});
 }
