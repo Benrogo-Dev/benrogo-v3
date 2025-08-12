7d6586b1b02b8d383c4103c57554bdc8fc95dc7c
BenRogozinski <brogodev@gmail.com>
2025-08-11 23:28:46 -0500

    IT BUILDSSSS

--- src/app/api/stats/route.ts
+++ src/app/api/stats/route.ts
@@ -1,22 +1,21 @@
+import { makeBigQueryRequest } from "@/utils/bigquery";
+import * as errors from "@/utils/errors";
 
-import { BigQuery } from "@google-cloud/bigquery";
+export const runtime = "edge";
 
 export const GET = async (): Promise<Response> => {
-  // Load credentials from environment variable
-  const credentials = process.env.GOOGLE_CREDENTIALS ? JSON.parse(process.env.GOOGLE_CREDENTIALS) : undefined;
-  const bigQueryClient = new BigQuery({
-    credentials,
-    projectId: credentials?.project_id,
-  });
+  const statsQuery = `
+    SELECT
+      SUM(requestCount) AS requests,
+      SUM(totalRequestLength) AS bytesIn,
+      SUM(totalResponseLength) AS bytesOut
+    FROM proxyAnalytics.combinedStats
+  `;
 
-  const query = `SELECT SUM(requestCount) AS requests, SUM(totalRequestLength) AS bytesIn, SUM(totalResponseLength) AS bytesOut FROM proxyAnalytics.combinedStats`;
-
-  const [statsJob] = await bigQueryClient.createQueryJob({
-    query,
-    location: "US"
-  });
-
-  const [[stats]] = await statsJob.getQueryResults();
-
-  return Response.json({stats}, {status: 200});
+  try {
+    const [stats] = await makeBigQueryRequest(statsQuery);
+    return Response.json(stats, {status: 200});
+  } catch (error) {
+    return errors.internal(error);
+  }
 }
