a93b74e1e3918f977dbe8ea907b6036abf6ecfcd
no. <169512939+1337Core@users.noreply.github.com>
2025-08-15 17:56:12 -0400

    oopsie

--- src/app/linkies/page.tsx
+++ src/app/linkies/page.tsx
@@ -1,56 +1,17 @@
-"use client";
-
 import type { Metadata } from "next";
-import { BasePage, LinkiesTable } from "@/components";
-import * as React from "react";
+import { BasePage } from "@/components";
+import LinkiesPageClient from "./LinkiesPageClient";
 
 export const metadata: Metadata = {
   title: "Linkies",
 };
 
-const Home = () => {
-  const [onlineOnly, setOnlineOnly] = React.useState(false);
-
-  const handleOnlineOnlyChange = (value: boolean) => {
-    setOnlineOnly(value);
-    // api req parims here pookie
-    console.log("Filter changed to onlineOnly:", value);
-  };
-
+const Page = () => {
   return (
     <BasePage>
-      <LinkiesTable
-        onlineOnly={onlineOnly}
-        onOnlineOnlyChange={handleOnlineOnlyChange}
-        linkData={[
-          {
-            url: "https://www.google.com",
-            requests: 38495723,
-            status: "online",
-            loadTime: 0.33,
-            reliability: 8.2,
-          },
-          {
-            url: "https://www.example.com",
-            requests: 2793,
-            status: "offline",
-            reliability: 1.7,
-          },
-          {
-            url: "https://benrogo.net",
-            requests: 129344,
-            status: "maintenance",
-            reliability: 4.2,
-          },
-          {
-            url: "https://discord.com",
-            requests: 91360,
-            status: "unknown",
-          },
-        ]}
-      />
+      <LinkiesPageClient />
     </BasePage>
   );
 };
 
-export default Home;
+export default Page;
